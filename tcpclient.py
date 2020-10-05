d382d1184dca0f0ad5596ab83d7c20ca532d4803
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2020-10-05 08:53:53 +0530

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -8,8 +8,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n'.encode())
+client.send('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n'.encode('utf-8'))
 
-response = client.recv(4096)
+response = client.recv(4096).decode('utf-8')
 
 print(response)
